10f45f7682a069552d172e70f31f0944bd8e465d
bbrodo <15292@coderacademy.edu.au>
2024-09-20 13:42:30 +1000

    feat: Added centered cards using bootstrap css

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,11 +1,20 @@
-
+import Card from "react-bootstrap/Card";
 
 export default function FoodCard(props){
+    const {foodName} = props
     return (
-        <div>
-            <h3>{props.foodName}</h3>
-            <p>Description</p>
-        </div>
+        <div className="Card">
+        <Card style={{ width: '18rem'}}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{foodName}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
     )
     
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
+
+
 export default function Header() {
     return (
-        <h1>React props Lesson</h1>
+        <div className="App-header">
+            <h1>React props Lesson</h1>
+        </div>
     )
 }
